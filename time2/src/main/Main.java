c27ecd29174d1c189f7f10696fdba6fab77b6562
Rasim'di <Rasim'di@DESKTOP-7J2T6HC>
2022-06-13 19:08:23 +0400

    Rasim'di File-io homework

--- time2/src/main/Main.java
+++ time2/src/main/Main.java
@@ -6,17 +6,20 @@ import java.util.stream.Collectors;
 public class Main {
 	public static void main(String[] args) {
 		  LocalDateTime localdate= LocalDateTime.now();
+		  System.out.println(localdate);
 		  String a = localdate.toString();
 		  String str[]=a.split("T");
-		  printStringg(str);
-		  List<String> strings = Pattern.compile("T").splitAsStream(a).collect(Collectors.toList());
-		  for(String string : strings) {
-			  System.out.println("date :"+string );
-		  }
+		  System.out.println("Date "+str[0]);
+		  System.out.println("Time "+str[1]);
+//		  printStringg(str);
+//		  List<String> strings = Pattern.compile("T").splitAsStream(a).collect(Collectors.toList());
+//		  for(String string : strings) {
+//			  System.out.println("date :"+string );
+//		  }
 		}
-		public static void printStringg(String str[]) {
-			for(String string : str) {
-				System.out.println(string);
-			}
-		}
+//		public static void printStringg(String str[]) {
+//			for(String string : str) {
+//				System.out.println(string);
+//			}
+//		}
 }
